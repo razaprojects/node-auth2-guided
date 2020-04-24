a2dfdaed718762fa5925cb6036c8ad5a0f5511b8
Mohd Raza <noorinfotechinc@gmail.com>
2020-04-24 10:54:44 -0500

    init project

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
@@ -11,11 +11,26 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
+server.get("/token", (req, res) => {
+  const payload = {
+    subject: "thisuser",
+    userid: "raza",
+    favoriteChili: "my favorite",
+  };
+
+  const secret = "mysecresl";
+  const options = {
+    expiresIn: "30m",
+  };
+
+  const token = jwt.sign(payload, secret, options);
+  res.json(token);
+});
 module.exports = server;
